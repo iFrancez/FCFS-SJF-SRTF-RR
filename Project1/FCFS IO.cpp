362d9f3aaec37e257fbb2a618ec70148009cd940
iFrancez <phannguyenthanhphap@gmail.com>
2023-03-20 18:21:21 +0700

    Fix FCFS

---
+++ Project1/FCFS IO.cpp
@@ -0,0 +1,106 @@
+﻿//#include <iostream>
+//#include <queue>
+//
+//using namespace std;
+//
+//struct Process {
+//    int pid; // Process ID
+//    int arrival_time; // Arrival time
+//    int burst_time; // Burst time
+//    int io_time; // I/O time
+//    int io_duration; // I/O duration
+//};
+//
+//int main() {
+//    int n; // Số lượng tiến trình
+//    cout << "Nhap so luong tien trinh: ";
+//    cin >> n;
+//
+//    // Tạo một hàng đợi lưu trữ các tiến trình
+//    queue<Process> process_queue;
+//
+//    // Nhập thông tin cho các tiến trình
+//    for (int i = 0; i < n; i++) {
+//        Process p;
+//        cout << "Nhap thong tin tien trinh " << i + 1 << ":\n";
+//        cout << "Nhap arrival time: ";
+//        cin >> p.arrival_time;
+//        cout << "Nhap burst time: ";
+//        cin >> p.burst_time;
+//        cout << "Nhap I/O time: ";
+//        cin >> p.io_time;
+//        cout << "Nhap I/O duration: ";
+//        cin >> p.io_duration;
+//        p.pid = i + 1;
+//        process_queue.push(p);
+//    }
+//
+//    int current_time = 0;  // Thời điểm hiện tại
+//    int total_waiting_time = 0;  // Tổng thời gian chờ đợi của các tiến trình
+//    queue<Process> io_queue;  // Hàng đợi I/O
+//
+//    // Xử lý các tiến trình
+//    while (!process_queue.empty() || !io_queue.empty()) {
+//        // Nếu hàng đợi tiến trình sẵn sàng rỗng
+//        if (process_queue.empty()) {
+//            current_time++;
+//            continue;
+//        }
+//
+//        // Lấy tiến trình đầu tiên từ hàng đợi tiến trình sẵn sàng để thực hiện
+//        Process current_process = process_queue.front();
+//        process_queue.pop();
+//
+//        // Nếu tiến trình đã đến thời điểm thực hiện
+//        if (current_time >= current_process.arrival_time) {
+//            // Thực hiện I/O
+//            if (current_time == current_process.io_time) {
+//                io_queue.push(current_process);
+//            }
+//            // Hoàn tất I/O, đưa tiến trình vào hàng đợi
+//            else if (current_time == current_process.io_time + current_process.io_duration) {
+//                current_process.io_time = -1; // Đánh dấu là tiến trình không còn thực hiện I/O nữa
+//                current_process.arrival_time = current_time; // Cập nhật lại thời gian đến của tiến trình
+//                process_queue.push(current_process);
+//            }
+//            // Thực hiện tiến trình
+//            else {
+//                current_process.burst_time--;
+//                // Nếu tiến trình đã hoàn thành
+//                if (current_process.burst_time == 0) {
+//                    int waiting_time = current_time - current_process.arrival_time;
+//                    total_waiting_time += waiting_time;
+//                }
+//                // Đưa tiến trình vào hàng đợi tiếp theo
+//                else {
+//                    process_queue.push(current_process);
+//                }
+//            }
+//        }
+//        // Tiến trình chưa đến
+//    // Xử lý I/O
+//        if (!io_queue.empty()) {
+//            Process io_process = io_queue.front();
+//            io_queue.pop();
+//            io_process.io_duration--;
+//            // Nếu I/O hoàn tất
+//            if (io_process.io_duration == 0) {
+//                io_process.io_time = -1; // Đánh dấu là tiến trình không còn thực hiện I/O nữa
+//                io_process.arrival_time = current_time; // Cập nhật lại thời gian đến của tiến trình
+//                process_queue.push(io_process);
+//            }
+//            // Nếu I/O chưa hoàn tất
+//            else {
+//                io_queue.push(io_process);
+//            }
+//        }
+//
+//        current_time++;
+//    }
+//
+//    // Tính thời gian trung bình chờ đợi của mỗi tiến trình
+//    float avg_waiting_time = (float)total_waiting_time / n;
+//    cout << "Thoi gian trung binh cho doi cua moi tien trinh: " << avg_waiting_time << endl;
+//
+//    return 0;
+//}
--- Project1/FCFS.cpp
+++ Project1/FCFS.cpp
@@ -1,108 +1,75 @@
-
-#include <iostream>
-#include <queue>
+#include<iostream>
 using namespace std;
 
-struct Process {
-    int pid; // process ID
-    int arrival_time; // arrival time of the process
-    int burst_time; // CPU burst time of the process
+struct process
+{
+    int pid;
+    int s_time;
+    int c_time;
+    int t_time;
+    int w_time;
+    int b_time;
+    int a_time;
+    int r_time;
+
 };
-
-void fcfs_scheduling(Process processes[], int n) {
-    queue<Process> ready_queue; // a queue to store the ready processes
-    int current_time = 0; // the current time
-    int total_waiting_time = 0; // the total waiting time of all processes
-    int total_turnaround_time = 0; // the total turnaround time of all processes
-    // save waiting and size 
-    int wait[100], count = 0;
-
-    //save turn - around
-    int around[100], countR = 0;
-
-    // loop through all processes
-    for (int i = 0; i < n; i++) {
-        // if the current time is less than the arrival time of the process, set the current time to the arrival time of the process
-        if (current_time < processes[i].arrival_time) {
-            current_time = processes[i].arrival_time;
-        }
-
-        // add the process to the ready queue
-        ready_queue.push(processes[i]);
-
-        // process the processes in the ready queue
-        while (!ready_queue.empty()) {
-            Process current_process = ready_queue.front();
-            ready_queue.pop();
-
-            // calculate the waiting time and turnaround time of the process
-            int waiting_time = current_time - current_process.arrival_time;
-            int turnaround_time = waiting_time + current_process.burst_time;
-
-            // add the waiting time and turnaround time to the total waiting time and total turnaround time
-            total_waiting_time += waiting_time;
-
-            //save and size waiting
-            wait[i] = waiting_time;
-            count++;
-
-            total_turnaround_time += turnaround_time;
-
-            // save an size around
-            around[i] = turnaround_time;
-            countR++;
-
-            // set the current time to the completion time of the process
-            current_time += current_process.burst_time;
-        }
-    }
-
-    // calculate the average waiting time and average turnaround time
-    double avg_waiting_time = (double)total_waiting_time / n;
-    double avg_turnaround_time = (double)total_turnaround_time / n;
-
-    // print the results
-    cout << endl;
-    cout << "Average waiting time: ";
-    for (int i = 0; i < count - 1; i++) {
-        cout << wait[i] << " + ";
-    }
-    cout << wait[count - 1] << " / " << n << " = " << avg_waiting_time << endl;
-
-
-    cout << "Average turnaround time: ";
-    for (int i = 0; i < countR - 1; i++) {
-        cout << around[i] << " + ";
-    }
-    cout << around[countR - 1] << " / " << n << " = " << avg_turnaround_time << endl;
+static bool compare(process a, process b)
+{
+    return a.a_time < b.a_time;
 }
-
-int main() {
-    int n; // number of processes
-    cout << "Enter the number of processes: " << endl;
+static bool compare2(process a, process b)
+{
+    return a.pid < b.pid;
+}
+int main()
+{
+    int n;
+    float avg_tt = 0, avg_wt = 0, total_tt = 0, total_wt = 0;
+    cout << "\n Enter No of Process";
     cin >> n;
+    process p[100];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "\nProcess " << i + 1 << "\n";
+        cout << "\nArrival Time: ";
+        cin >> p[i].a_time;
+        cout << "\nBurst Time: ";
+        cin >> p[i].b_time;
+        p[i].pid = i + 1;
 
-    Process processes[100]; // an array to store the processes
-
-    // get the arrival time and burst time of each process
-    for (int i = 0; i < n; i++) {
-        cout << "Enter the arrival time and burst time of process " << i << ": " << endl;
-        cin >> processes[i].arrival_time >> processes[i].burst_time;
-        processes[i].pid = i;
     }
+    //sort(p, p + n, compare);
+    for (int i = 0; i < n; i++)
+    {
+        p[i].s_time = (i == 0) ? p[i].a_time : p[i - 1].c_time;
+        p[i].c_time = p[i].s_time + p[i].b_time;
+        p[i].t_time = p[i].c_time - p[i].a_time;
+        p[i].w_time = p[i].t_time - p[i].b_time;
+        p[i].r_time = p[i].s_time - p[i].a_time;
 
-    // perform FCFS scheduling
-    fcfs_scheduling(processes, n);
+        total_wt += p[i].w_time;
+        total_tt += p[i].t_time;
 
+    }
+    avg_wt = (total_wt / n);
+    avg_tt = (total_tt / n);
+    //sort(p, p + n, compare2);
+    cout << "\nPID\t\tAT\t\tBT\t\tTAT\t\tWT";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "\n" << p[i].pid << "\t\t" << p[i].a_time << "\t\t" << p[i].b_time << "\t\t" << p[i].t_time << "\t\t" << p[i].w_time;
+
+    }
+    cout << "\nAverage Waiting Time :" << avg_wt;
+    cout << "\nAverage TurnAround Time :" << avg_tt;
     return 0;
 }
-
 
 /*
 5
 0 10
-1 1
-2 2
-3 1
-4 5
+0 5
+0 14
+0 7
+0 13
 */
